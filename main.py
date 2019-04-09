9d84613de4f4533fecfa977170959b0fcdd96d4b
Sengkhun Lim <sengkhunlim@Sengkhuns-MacBook-Pro.local>
2019-04-09 22:18:25 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,42 @@
+import math
+
+def find_tf_idf(line):
+  value = line.rstrip("\n\r").split(',')
+  keyword = value[0]
+  d1 = int(value[1])
+  d2 = int(value[2])
+  d3 = int(value[3])
+
+  idf = find_idf(value[1:])
+
+  print('{:10} '.format(keyword), end='')
+  for x in value[1:]:
+    tf = find_tf(x)
+    tf_idf = tf * idf
+    if tf_idf > 0:
+      print('{:.2f}  '.format(tf_idf), end='')
+    else:
+      print('{:6}'.format(''), end='')
+      
+  print()
+
+def find_tf(d):
+  # casting
+  int_d = int(d)
+  return 1 + math.log2(int_d) if int_d > 0 else 0
+
+def find_idf(d):
+  N = len(d)
+  ni = 0
+  for x in d:
+    if int(x) > 0:
+      ni += 1
+  return math.log2(N/ni)
+
+f = open("data.txt", "r")
+data = f.readlines()
+
+print('\n===Document Matrix===')
+for line in data:
+  find_tf_idf(line)
+print()
